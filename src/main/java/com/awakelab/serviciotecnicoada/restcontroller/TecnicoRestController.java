4a622025426b09e26df912f9ea43c80fbca8fd0b
jdavidlucios <juandalucio@gmail.com>
2023-08-31 21:14:34 -0400

    creación  T©cnico Rest Controller

--- src/main/java/com/awakelab/serviciotecnicoada/restcontroller/TecnicoRestController.java
+++ src/main/java/com/awakelab/serviciotecnicoada/restcontroller/TecnicoRestController.java
@@ -1,4 +1,31 @@
 package com.awakelab.serviciotecnicoada.restcontroller;
 
+import com.awakelab.serviciotecnicoada.entity.Tecnico;
+import com.awakelab.serviciotecnicoada.service.ITecnicoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/usuario")
 public class TecnicoRestController {
+
+    @Autowired
+    ITecnicoService objTecnicoService;
+
+    @GetMapping("/{id}")
+    public Tecnico listarTecnicoId(@PathVariable int id) {
+        return objTecnicoService.listarTecnicoID(id);
+    }
+
+    @PostMapping
+    public Tecnico crearTecnico(@RequestBody Tecnico tecnico){
+        return objTecnicoService.crearTecnico(tecnico);
+
+    }
+    @GetMapping
+    public List<Tecnico> listarTecnicos(){
+        return objTecnicoService.listarTecnico();
+    }
 }
