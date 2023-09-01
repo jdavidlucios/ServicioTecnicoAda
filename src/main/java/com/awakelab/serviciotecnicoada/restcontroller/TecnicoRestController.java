11feada81be36702c4c7e123146e97607a11db31
Pablo Aliaga <pandresaliaga@gmail.com>
2023-08-31 20:47:18 -0400

    rest controller de tecnico y cliente

--- src/main/java/com/awakelab/serviciotecnicoada/restcontroller/TecnicoRestController.java
+++ src/main/java/com/awakelab/serviciotecnicoada/restcontroller/TecnicoRestController.java
@@ -1,4 +1,31 @@
 package com.awakelab.serviciotecnicoada.restcontroller;
 
+import com.awakelab.serviciotecnicoada.entity.Tecnico;
+import com.awakelab.serviciotecnicoada.service.IUsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/tecnico")
 public class TecnicoRestController {
+
+    @Autowired
+    ITecnicoService objTecnicoService;
+
+    @GetMapping("/{id}")
+    public Tecnico listarTecnicoID(@PathVariable int id)  { return objTecnicoService.listarTecnicoID(id);
+    }
+
+    @PostMapping
+    public TecnicoRestController crearTecnico(@RequestBody Tecnico nuevoTecnico) {
+        return objTecnicoService.crearTecnico(tecnico);
+    }
+
+    @GetMapping
+    public List<Tecnico> listarTecnico(){
+        return objTecnicoService.listarTecnico();
+    }
+
+    private class ITecnicoService {
+    }
 }
